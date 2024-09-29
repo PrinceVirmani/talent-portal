4e8590c96ab5c15beac4d5c389c45533a679eb4c
Prince <princevirmani410@gmail.com>
2024-09-29 09:37:17 +0530

    onboarding

--- src/Pages/Onboarding.jsx
+++ src/Pages/Onboarding.jsx
@@ -1,7 +1,59 @@
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { BarLoader } from "react-spinners";
 
 function Onboarding() {
-  return <div>Onboarding</div>;
+  const { user, isLoaded } = useUser();
+  const navigate = useNavigate();
+  const navigateUser = (currRole) => {
+    navigate(currRole === "recruiter" ? "/post-job" : "/jobs");
+  };
+  const handleRoleSelection = async (role) => {
+    await user
+      .update({
+        unsafeMetadata: { role },
+      })
+      .then(() => {
+        console.log(`Role updated to: ${role}`);
+        navigateUser(role);
+      })
+      .catch((err) => {
+        console.error("Error Updating role :", err);
+      });
+  };
+
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+  return (
+    <div className="flex flex-col items-center justify-center mt-32">
+      <h2 className="gradient-title font-extrabold text-7xl sm:text-8xl tracking-tighter">
+        I am ...
+      </h2>
+      <div className="mt-16 grid grid-cols-2 gap-4 w-full md:px-40">
+        <Button
+          variant="blue"
+          className="h-36 text-2xl"
+          onclick={() => {
+            handleRoleSelection("candidate");
+          }}
+        >
+          Candidate
+        </Button>
+        <Button
+          variant="destructive"
+          className="h-36 text-2xl"
+          onclick={() => {
+            handleRoleSelection("recruiter");
+          }}
+        >
+          Recruiter
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Onboarding;
