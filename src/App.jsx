d018ceebf27cf0fc1c25e2d3e1ffeb7a2c0fdfea
Prince <princevirmani410@gmail.com>
2024-10-02 23:18:54 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./App.css";
-import { Button } from "./components/ui/button";
-import AppLayout from "./Layouts/AppLayout";
-import LandingPage from "./Pages/LandingPage";
-import Onboarding from "./Pages/Onboarding";
-import JobListing from "./Pages/JobListing";
-import MyJobs from "./Pages/MyJobs";
-import JobPage from "./Pages/JobPage";
-import PostJobs from "./Pages/PostJobs";
-import SavedJobs from "./Pages/SavedJobs";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import AppLayout from "./layouts/app-layout";
+import ProtectedRoute from "./components/protected-route";
 import { ThemeProvider } from "./components/theme-provider";
-import ProtectedRoutes from "./components/ProtectedRoutes";
+
+import LandingPage from "./pages/landing";
+import Onboarding from "./pages/onboarding";
+import PostJob from "./pages/post-job";
+import JobListing from "./pages/jobListing";
+import MyJobs from "./pages/my-jobs";
+import SavedJobs from "./pages/saved-jobs";
+import JobPage from "./pages/job";
+
+import "./App.css";
 
 const router = createBrowserRouter([
   {
@@ -23,49 +25,49 @@ const router = createBrowserRouter([
       {
         path: "/onboarding",
         element: (
-          <ProtectedRoutes>
-            <Onboarding />,
-          </ProtectedRoutes>
+          <ProtectedRoute>
+            <Onboarding />
+          </ProtectedRoute>
         ),
       },
       {
-        path: "/joblisting",
+        path: "/jobs",
         element: (
-          <ProtectedRoutes>
-            <JobListing />,
-          </ProtectedRoutes>
+          <ProtectedRoute>
+            <JobListing />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/post-job",
+        element: (
+          <ProtectedRoute>
+            <PostJob />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/my-jobs",
+        element: (
+          <ProtectedRoute>
+            <MyJobs />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/saved-jobs",
+        element: (
+          <ProtectedRoute>
+            <SavedJobs />
+          </ProtectedRoute>
         ),
       },
       {
         path: "/job/:id",
         element: (
-          <ProtectedRoutes>
-            <JobPage />,
-          </ProtectedRoutes>
-        ),
-      },
-      {
-        path: "/myjobs",
-        element: (
-          <ProtectedRoutes>
-            <MyJobs />,
-          </ProtectedRoutes>
-        ),
-      },
-      {
-        path: "/postjobs",
-        element: (
-          <ProtectedRoutes>
-            <PostJobs />,
-          </ProtectedRoutes>
-        ),
-      },
-      {
-        path: "/savedjobs",
-        element: (
-          <ProtectedRoutes>
-            <SavedJobs />,
-          </ProtectedRoutes>
+          <ProtectedRoute>
+            <JobPage />
+          </ProtectedRoute>
         ),
       },
     ],
--- src/Pages/Onboarding.jsx
+++ src/Pages/Onboarding.jsx
@@ -1,59 +1,62 @@
-import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
-import React from "react";
+import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import { BarLoader } from "react-spinners";
 
-function Onboarding() {
+const Onboarding = () => {
   const { user, isLoaded } = useUser();
   const navigate = useNavigate();
+
   const navigateUser = (currRole) => {
     navigate(currRole === "recruiter" ? "/post-job" : "/jobs");
   };
+
   const handleRoleSelection = async (role) => {
     await user
-      .update({
-        unsafeMetadata: { role },
-      })
+      .update({ unsafeMetadata: { role } })
       .then(() => {
         console.log(`Role updated to: ${role}`);
         navigateUser(role);
       })
       .catch((err) => {
-        console.error("Error Updating role :", err);
+        console.error("Error updating role:", err);
       });
   };
+
+  useEffect(() => {
+    if (user?.unsafeMetadata?.role) {
+      navigateUser(user.unsafeMetadata.role);
+    }
+  }, [user]);
 
   if (!isLoaded) {
     return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
   }
+
   return (
-    <div className="flex flex-col items-center justify-center mt-32">
+    <div className="flex flex-col items-center justify-center mt-40">
       <h2 className="gradient-title font-extrabold text-7xl sm:text-8xl tracking-tighter">
-        I am ...
+        I am a...
       </h2>
       <div className="mt-16 grid grid-cols-2 gap-4 w-full md:px-40">
         <Button
           variant="blue"
           className="h-36 text-2xl"
-          onclick={() => {
-            handleRoleSelection("candidate");
-          }}
+          onClick={() => handleRoleSelection("candidate")}
         >
           Candidate
         </Button>
         <Button
           variant="destructive"
           className="h-36 text-2xl"
-          onclick={() => {
-            handleRoleSelection("recruiter");
-          }}
+          onClick={() => handleRoleSelection("recruiter")}
         >
           Recruiter
         </Button>
       </div>
     </div>
   );
-}
+};
 
 export default Onboarding;
